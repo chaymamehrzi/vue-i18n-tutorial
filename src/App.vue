2de682f66d9e8ec9111eb31cc068a8459f233510
chaymamehrzi <chayma.mehrzi@esprit.tn>
2024-01-29 12:04:36 +0100

    initial changes

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1> Vue Internationalization App</h1>
+    <HelloI18n />
+  </div>
+</template>
+
+<script>
+import HelloI18n from "./components/HelloI18n.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    HelloI18n,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
